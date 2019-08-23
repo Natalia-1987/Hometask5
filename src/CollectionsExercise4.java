6ea255c17f9e8b7515f2a3864ccbf494a46707c6
Natalia-1987 <ndomanska@gmail.com>
2019-08-23 17:02:37 +0300

    commit6

---
+++ src/CollectionsExercise4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class CollectionsExercise4 {
+    public static void main(String[] args) {
+        ArrayList<String> mylist = new ArrayList<String>();
+        mylist.add("Norway");
+        mylist.add("Germany");
+        mylist.add("Austria");
+        mylist.add("Spain");
+        mylist.add("Ukraine");
+
+        System.out.println("Original List : \n" + mylist);
+        Collections.shuffle(mylist);
+        System.out.println("\nShuffled List : \n" + mylist);
+
+
+
+
+        LinkedList<String> mylist2 = new LinkedList<String>();
+        mylist.add("Norway");
+        mylist.add("Germany");
+        mylist.add("Austria");
+        mylist.add("Spain");
+        mylist.add("Ukraine");
+        
+        System.out.println("Original List : \n" + mylist2);
+        Collections.shuffle(mylist2);
+        System.out.println("\nShuffled List : \n" + mylist2);
+    }
+}
